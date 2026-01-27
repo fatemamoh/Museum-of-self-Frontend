82fe7492fa9fdf5f4410b6c287176da26c4f79e6
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-27 19:46:46 +0300

    theme

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
 import { UserProvider } from './contexts/UserContext'
 import './index.css';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
+      <UserProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
 )
