1744e02b6f011190b0ac82eeea29f343f0d1c88e
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-27 10:54:31 +0300

    forgot password form

--- src/components/Auth/ForgotPassword.jsx
+++ src/components/Auth/ForgotPassword.jsx
@@ -6,9 +6,21 @@ import React from 'react'
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
     const [message,setMessage] = useState('');
+
+
   return (
-    <div>ForgotPassword</div>
-  )
-}
+<main>
+    <h1>Vault Recovery</h1>
+    <p>{message}</p>
+    <form onSubmit={}>
+    <label htmlFor="email">Email Adress:</label>
+    <input type="email" id="email" value={email} placeholder="Enter your registered email" 
+    required onChange={}/>
+    <button type="submit">Send Reset Link</button>
+
+    </form>
+</main>
+)
+};
 
 export default ForgotPassword
