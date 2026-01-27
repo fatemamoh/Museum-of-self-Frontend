08cd18a2674c64447107ea66985dc4d7724cb1db
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-27 11:12:21 +0300

    navigation

--- src/components/Auth/ForgotPassword.jsx
+++ src/components/Auth/ForgotPassword.jsx
@@ -1,30 +1,43 @@
 import { useState } from "react";
 import { forgotPassword } from "../../services/authService";
-
-import React from 'react'
+import { Link } from "react-router";
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
-    const [message,setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
     const handleChange = (e) => {
         setMessage('');
         setEmail(e.target.value);
     };
 
-  return (
-<main>
-    <h1>Vault Recovery</h1>
-    <p>{message}</p>
-    <form onSubmit={}>
-    <label htmlFor="email">Email Adress:</label>
-    <input type="email" id="email" value={email} placeholder="Enter your registered email" 
-    required onChange={handleChange}/>
-    <button type="submit">Send Reset Link</button>
+    const handleSubmit = async (e) => {
+        e.preventdefault();
+        try {
+            const responce = await forgotPassword(email);
+            setMessage(responce.message);
+        } catch (error) {
+            setMessage(err.err || 'Error sending request');
+        }
+    }
 
-    </form>
-</main>
-)
+    return (
+        <main>
+            <h1>Vault Recovery</h1>
+            {message && <p>{message}</p>}
+            {!message ? (
+            <form onSubmit={ handleSubmit }>
+                <label htmlFor="email">Email Adress:</label>
+                <input type="email" id="email" value={email} placeholder="Enter your registered email"
+                    required onChange={ handleChange } />
+                <button type="submit">Send Reset Link</button>
+            </form>
+            ) : (
+                <p>Please check your inbox for further instructions.</p>
+            )}
+                <Link to="/sign-in">Return to Entrance (Sign In)</Link>
+        </main>
+    )
 };
 
 export default ForgotPassword
