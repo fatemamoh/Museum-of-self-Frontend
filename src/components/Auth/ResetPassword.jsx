a9ab9d888cc64fa0c7ceec45ac21d2a9e77ad7ef
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-27 11:18:57 +0300

    reset form

---
+++ src/components/Auth/ResetPassword.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router";
+import { resetPassword } from "../../services/authService";
+
+
+const ResetPassword = () => {
+  return (
+    <div>ResetPassword</div>
+  )
+}
+
+export default ResetPassword
